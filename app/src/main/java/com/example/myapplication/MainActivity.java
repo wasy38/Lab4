add1b59b7fb47e5183b1c861fbd49f80b0168905
Okhotikov Pavel <alex5perevalov@mail.ru>
2021-11-25 13:48:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView)findViewById(R.id.animationView);
+        img.setBackgroundResource(R.drawable.rabbit_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.setOneShot(false);
+        frameAnimation.start();
+
+    }
+
+}
