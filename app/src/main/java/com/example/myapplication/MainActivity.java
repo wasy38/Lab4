9ecf9ab2c7056d86d1de9f97baf0c38660504649
Okhotikov Pavel <alex5perevalov@mail.ru>
2021-12-16 07:16:15 +0700

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        ImageView img = (ImageView)findViewById(R.id.animationView);
+    }
+    public void btn_click(View view)
+    {
+        ImageView img = (ImageView)findViewById(R.id.imageView);
         img.setBackgroundResource(R.drawable.rabbit_animation);
         AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
         frameAnimation.setOneShot(false);
         frameAnimation.start();
-
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setText("Кролик идет");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(frameAnimation.isRunning()) {
+                    frameAnimation.stop();
+                    btn.setText("Кролик стоит");
+                }
+                else {
+                    frameAnimation.start();
+                    btn.setText("Кролик идет");
+                }
+            }
+        });
     }
-
 }
